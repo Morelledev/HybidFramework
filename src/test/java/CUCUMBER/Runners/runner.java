ec1d1b88604e0c332eb1ed16abf84ff88acce0ad
Cele <njceles@gmail.com>
2024-03-18 19:59:44 +0200

    building cucumber framework

--- src/test/java/CUCUMBER/Runners/runner.java
+++ src/test/java/CUCUMBER/Runners/runner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "C:\\Users\\Cele\\Documents\\GitProjects\\GitHub\\HybridFramework\\src\\test\\java\\CUCUMBER\\Features",
+        features = "src/test/java/CUCUMBER/Features/",
         glue = {"StepDefinations"},
         tags = "@Login",
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
